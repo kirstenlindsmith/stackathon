d4dd62c536c761a61ca0a006b4d0abaeabfb73c6
kirstenlindsmith <kirstenlindsmith@gmail.com>
2019-03-11 11:02:30 -0400

    pause

---
+++ backburner/prototypeIndex.js
@@ -0,0 +1,20 @@
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 8080
+// const app = express()
+
+// // app.use(express.static(path.join(__dirname, '.', '/index.html')))
+
+// app.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '.', '/index.html'))
+// })
+
+// app.listen(PORT, ()=> {
+//   console.log(`Listening on port ${PORT}!`)
+// })
+
+// // express()
+// //   .use(express.static(path.join(__dirname, 'index')))
+// //   .set('view engine', 'html')
+// //   .get('/', (req, res) => res.render('index'))
+// //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
